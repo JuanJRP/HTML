fc6c74826256ee4fc6a1a1da3ea8244b58d04715
JuanJRP <juanjoserestrepopabon@gmail.com>
2022-04-05 15:37:43 -0500

    Finalizado

---
+++ Untitled-1.js
@@ -0,0 +1,22 @@
+var n1 = prompt("Ingrese el dia: ");
+if (n1 == "lunes"){
+    alert("Trabajar y estudiar");
+}
+if (n1 == "martes"){
+    alert("Trabajar y estudiar");
+}
+if (n1 == "miercoles"){
+    alert("Trabajar y estudiar");
+}
+if (n1 == "jueves"){
+    alert("Trabajar y estudiar");
+}
+if (n1 == "viernes"){
+    alert("Trabajar y estudiar");
+}
+if (n1 == "sabado"){
+    alert("Trabajar");
+}
+if (n1 == "domingo"){
+    alert("Dormir");
+}
